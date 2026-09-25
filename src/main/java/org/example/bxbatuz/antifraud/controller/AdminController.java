ca9a7d4dbcf2fcdd223314ec13d1f6916dfd4fbd
Islom <153659055+islom0202@users.noreply.github.com>
2026-09-25 11:52:07 +0500

    Add AdminService dependency to AdminController

--- src/main/java/org/example/bxbatuz/antifraud/controller/AdminController.java
+++ src/main/java/org/example/bxbatuz/antifraud/controller/AdminController.java
@@ -19,6 +19,7 @@ import java.util.List;
 @RequestMapping("/api/admin")
 @RequiredArgsConstructor
 public class AdminController {
+    
     private final AdminService adminService;
 
     @PostMapping("/create")
